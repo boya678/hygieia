a4298e027b3711878dc262312d12954da547d57b
Julian Velez <julian.velez@appgate.com>
2021-08-31 09:01:21 -0500

    first

--- Hygieia_Collector_vsts-build/src/main/java/com/capitalone/dashboard/repository/VSTSBuildCollectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.model.VSTSBuildCollector;
-
-/**
- * Collector repository for the BambooCollector subclass
- */
-public interface VSTSBuildCollectorRepository extends BaseCollectorRepository<VSTSBuildCollector> {
-}
--- Hygieia_Collector_vsts-build/src/main/java/com/capitalone/dashboard/repository/VSTSBuildJobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.model.VSTSBuildJob;
-
-
-public interface VSTSBuildJobRepository extends JobRepository<VSTSBuildJob> {
-	
-}
